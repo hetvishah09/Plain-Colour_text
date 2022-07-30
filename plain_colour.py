419a72825accd826a39ca92af2e5d90e70897c25
hetvishah09 <98590708+hetvishah09@users.noreply.github.com>
2022-07-30 15:42:33 +0530

    Add files via upload

---
+++ plain_colour.py
@@ -0,0 +1,8 @@
+import colorama
+
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.CYAN+Back.YELLOW+"Hi My name is Hetvi Shah"+ Fore.YELLOW+ Back.BLUE+"I am your Machine Learning Instructor")
+print(Back.CYAN+"Hi My name is Shah Hetvi")
+print(Fore.RED+ Back.GREEN+ "Hi My name is Shah Happy")
